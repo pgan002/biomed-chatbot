bb433ef4c270cba25c7327a73cb31040450bcd7d
Philip Ganchev <phil.ganchev@gmail.com>
2024-07-30 19:35:57 -0400

    Missing imports; fix naming; lazy load; check header; call model.get_mean_vector(); refactor

--- embedding_functions/glove.py
+++ embedding_functions/glove.py
@@ -1,5 +1,8 @@
+import sys
 from pathlib import Path
 
+import nltk
+import numpy
 from chromadb.api.types import (
     Document,
     Documents,
@@ -9,30 +12,48 @@ from chromadb.api.types import (
 )
 from gensim.models import KeyedVectors
 
+from utils import download_unzip
 
-NAME = 'glove.840B.300d'
-FILE_NAME_TEMPLATE = '{name}.txt'
+
+FILE_PATH = 'data/glove.840B.300d.txt'
 URL_TEMPLATE = 'https://downloads.cs.stanford.edu/nlp/data/{name}.zip'
 
 
-def get_url(name: str) -> str:
-    return URL_TEMPLATE.format(name=name)
+def get_url(file_path: str) -> str:
+    return URL_TEMPLATE.format(name=Path(file_path).stem())
 
 
-def get_file_path(name: str, dir_path: str = 'data') -> str:
-    fn = FILE_NAME_TEMPLATE.format(name=name)
-    return str(Path(dir_path, fn))
+def check_header(file_path: str) -> bool:
+    with open(file_path) as f:
+        for line in f:
+            break
+    values = line.split()
+    return len(values) == 2 and values[0].isdigit() and values[1].isdigit()
 
 
 class GloveEmbeddingFunction(EmbeddingFunction[Documents]):
-    def __init__(self, file_path: str = get_file_path(NAME)):
-        self.model = KeyedVectors.load_word2vec_format(file_path)
-
-    def embed_word(self, word: str) -> Embedding:
-        return self.model.get_vector(word).tolist()
+    def __init__(self, file_path: str = None):
+        self.file_path = file_path
+        self._model = None
     
+    def load(self):
+        if self.file_path is None:
+            self.file_path = FILE_PATH
+            if not Path(FILE_PATH).exists():
+                download_unzip(get_url(self.file_path))
+        no_header = not check_header(self.file_path)
+        print(f'{self.__class__.__name__}: load embedding file "{self.file_path}"')
+        self._model = KeyedVectors.load_word2vec_format(self.file_path, no_header=no_header)
+
+    @property
+    def model(self) -> KeyedVectors:
+        if self._model is None:
+            self.load()
+        return self._model
+
     def embed_text(self, text: Document) -> Embedding:
-        return numpy.mean([self.model.get_vector(word) for word in text.split()])
+        tokens = nltk.tokenize.word_tokenize(text)
+        return self.model.get_mean_vector(tokens).tolist()
     
     def __call__(self, input_: Documents) -> Embeddings:
         """Embed the input documents."""
