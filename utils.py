b89f1a557b0ee63b828c5963884d8706d8dd3f1f
Philip Ganchev <phil.ganchev@gmail.com>
2024-07-30 19:28:03 -0400

    Fix utils.unload_unzip()

--- utils.py
+++ utils.py
@@ -1,35 +1,26 @@
-from itertools import chain
+import os
+import zipfile
 import zlib
-
-
-def unzip(file_path):
-    dec = zlib.decompressobj(zlib.MAX_WBITS + 32)
-    with open(file_path, 'rb') as f:
-        for chunk in f:
-            rv = dec.decompress(chunk)
-            if rv:
-                f.write(rv)
-        if dec.unused_data:
-            f.write(dec.flush())
+from urllib.request import urlretrieve, urlopen
 
 
 def download_unzip(url: str, file_path: str):
-    dec = zlib.decompressobj(0)            
+    print(f'Downloading from {url}...')
+    zip_file_path = os.path.join(os.path.dirname(file_path), os.path.basename(url))
+    urlretrieve(url, zip_file_path)
+    with zipfile.ZipFile(zip_file_path) as f:
+        f.extractall(out_file_path)
+    os.remove(zip_file_path)
+
+
+def download_unzip_streaming(url: str, file_path: str):
+    '''Broken: dec.decompress() throws error regardless of wbits param'''
+    print(f'Downloading from {url}...')
+    dec = zlib.decompressobj(zlib.MAX_WBITS + 32)
     with urlopen(url) as stream:
          with open(file_path, 'wt') as f:
             for chunk in stream:
                 rv = dec.decompress(chunk)
-                if rv:
-                    f.write(rv)
-            if dec.unused_data:
-                f.write(dec.flush())
-
-
-def download_unzip(url: str, file_path: str):
-    with urlopen(url) as stream:
-         with open(file_path, 'wt') as f:
-            for chunk in stream:
-                rv = zlib.decompress(chunk, wbits=0)
                 if rv:
                     f.write(rv)
             if dec.unused_data:
